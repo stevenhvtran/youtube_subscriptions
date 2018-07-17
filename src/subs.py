127eddce867e3877f5ef031f94bac60c6835e199
stevenhvtran <stransteven@outlook.com>
2018-07-17 11:31:38 +1000

    Added sample for getting sub list

--- src/subs.py
+++ src/subs.py
@@ -3,6 +3,8 @@ import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+from pprint import pprint
+
 CLIENT_SECRETS_FILE = 'client_secret.json'
 
 # This OAuth 2.0 access scope allows for full read/write access to the
@@ -29,6 +31,15 @@ def channels_list_by_username(service, **kwargs):
            results['items'][0]['statistics']['viewCount']))
 
 
+# Sample python code for subscriptions.list
+def subscriptions_list_my_subscriptions(client, **kwargs):
+    response = client.subscriptions().list(
+        **kwargs
+    ).execute()
+
+    pprint(response)
+
+
 if __name__ == '__main__':
     # When running locally, disable OAuthlib's HTTPs verification. When
     # running in production *do not* leave this option enabled.
@@ -37,3 +48,6 @@ if __name__ == '__main__':
     channels_list_by_username(service,
                               part='snippet,contentDetails,statistics',
                               forUsername='GoogleDevelopers')
+    subscriptions_list_my_subscriptions(service,
+                                        part='snippet,contentDetails',
+                                        mine=True)
